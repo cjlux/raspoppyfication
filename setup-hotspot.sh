1799d461ac4482977838563aecf733fa694b1e60
Jean-Luc Charles <jean-luc.charles@ensam.eu>
2020-04-07 21:29:03 +0200

    Same processing for RPi3/RPi4

--- setup-hotspot.sh
+++ setup-hotspot.sh
@@ -1,37 +1,18 @@
 #!/bin/bash
 
-# modified vy JLC for RPi4 2020-02-17
+# modified by JLC to use rpi3-4-hotspot.zip for Rapbian-buster :
 
-if [ "$(hrpi-version)" = "rpi-3" ]; then
-    cd /tmp || exit
-    wget -O rpi3-hotspot.zip "https://gitlab.inria.fr/dcaselli/rpi3-hotspot/repository/archive.zip?ref=3.0.1"
-    unzip rpi3-hotspot.zip
-    rm rpi3-hotspot.zip
-    mv rpi3-hotspot* rpi3-hotspot
-    cd rpi3-hotspot || exit
-    ./install.sh
-    cd /tmp || exit
-    rm -rf rpi3-hotspot
+cd /tmp || exit
+wget -O rpi3-4-hotspot.zip "https://github.com/cjlux/raspoppyfication/raw/master/rpi3-4-hotspot.zip"
+unzip rpi3-4-hotspot.zip
+rm rpi3-4-hotspot.zip
+mv rpi3-4-hotspot-* rpi3-hotspot
+cd rpi3-hotspot || exit
+./install.sh
+cd /tmp || exit
+rm -rf rpi3-hotspot
 
-    tee /boot/hotspot.txt <<EOF
+tee /boot/hotspot.txt <<EOF
 ssid=Poppy Hotspot
 passphrase=poppyproject
 EOF
-elif [ "$(hrpi-version)" = "rpi-4" ]; then
-    cd /tmp || exit
-    wget -O rpi3-4-hotspot.zip "https://github.com/cjlux/raspoppyfication/raw/master/rpi3-4-hotspot.zip"
-    unzip rpi3-4-hotspot.zip
-    rm rpi3-4-hotspot.zip
-    mv rpi3-4-hotspot-* rpi3-hotspot
-    cd rpi3-hotspot || exit
-    ./install.sh
-    cd /tmp || exit
-    rm -rf rpi3-hotspot
-
-    tee /boot/hotspot.txt <<EOF
-ssid=Poppy Hotspot
-passphrase=poppyproject
-EOF
-fi
-
-
