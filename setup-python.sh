45a82f12f34817d288e5e2fd8bbc025026a7f8f9
Jean-Luc Charles <jean-luc.charles@ensam.eu>
2020-04-09 13:22:10 +0200

    Pillow -> pillow

--- setup-python.sh
+++ setup-python.sh
@@ -23,7 +23,7 @@ create_virtual_python_env()
 install_python_packages()
 {
     source $HOME/pyenv/bin/activate && pip install \
-    	numpy scipy==1.3.1 jupyter matplotlib explauto wheel Pillow\
+    	numpy scipy==1.3.1 jupyter matplotlib explauto wheel pillow\
     	opencv-contrib-python==4.1.0.25 
 }
 
